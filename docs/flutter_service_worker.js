f4f79419b4e526839808d4db65e734be4e562ed9
maliksenpai <cramixzx@hotmail.com>
2022-05-07 21:15:49 +0300

    - Update gh-pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -21,9 +21,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4cce45329c78107864950fcef96921a6",
-"/": "4cce45329c78107864950fcef96921a6",
-"main.dart.js": "34e29b11919d7813274c293e3d682c99",
+"index.html": "74e5fc9b6222968741ac4b8ada282b1b",
+"/": "74e5fc9b6222968741ac4b8ada282b1b",
+"main.dart.js": "95c4a7ecbfdb0c84f96a7b4fad16cf46",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"
 };
